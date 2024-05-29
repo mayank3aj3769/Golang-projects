4dcb708f641f72c3738dfc356a84675b61653c6a
Mayank Raj <mayank.raj3769@gmail.com>
2024-05-29 05:42:00 +0530

    playing with channels

--- go-channels/server-example/main.go
+++ go-channels/server-example/main.go
@@ -11,29 +11,29 @@ type Server struct {
 	quitch chan struct{}
 }
 
-func main() {
-	server := NewServer()
-	server.Start()
+// func main() {
+// 	server := NewServer()
+// 	server.Start()
 
-	// user, ok := <-server.userch // This can be used to see
-	//	whether the channel has been closed by a different process or not
+// 	// user, ok := <-server.userch // This can be used to see
+// 	//	whether the channel has been closed by a different process or not
 
-	// The following routing is just made to quit the loop for now.
-	go func() {
-		time.Sleep(2 * time.Second)
-		//server.quitch <- struct{}{} // does the same thing as close
-		close(server.quitch)
-	}()
+// 	// The following routing is just made to quit the loop for now.
+// 	go func() {
+// 		time.Sleep(2 * time.Second)
+// 		//server.quitch <- struct{}{} // does the same thing as close
+// 		close(server.quitch)
+// 	}()
 
-	// This blocks forever until at least one of the select cases(channels) becomes valid.
-	// A default case can be used to make it non-blocking
-	select {}
+// 	// This blocks forever until at least one of the select cases(channels) becomes valid.
+// 	// A default case can be used to make it non-blocking
+// 	select {}
 
-}
+// }
 func NewServer() *Server { /* Constructor for the struct Server*/
 	return &Server{
 		users:  make(map[string]string),
-		userch: make(chan string),
+		userch: make(chan string, 2),
 		quitch: make(chan struct{}),
 	}
 }
@@ -59,6 +59,7 @@ free:
 			fmt.Println("Server need to quit")
 			break free
 		default:
+
 		}
 	}
 }
@@ -67,26 +68,54 @@ func (s *Server) addUser(user string) {
 	s.users[user] = user
 }
 
-// func main() {
+func main() {
 
-// 	ch1 := make(chan string, 2)
-// 	ch1 <- "Hello"
-// 	sendMessage(ch1)
-// 	receiveMessage(ch1)
+	ch1 := make(chan string, 2)
+	ch1 <- "Hello"
+	sendMessage(ch1)
+	receiveMessage(ch1)
 
-// 	sr := NewServer()
-// 	//ch2 := make(chan string, 2)
-// 	sr.userch <- "Hey"
-// 	sr.addUser("Hey ")
-// 	sr.users["Hello"] = "Mayank"
-// 	user := <-sr.userch
-// 	fmt.Println(user)
-// 	sr.users["Hi"] = "Bob"
+	sr := NewServer()
+	sr.userch <- "Server ch content 1 "
+	sr.userch <- "Server ch content 2"
+	sr.addUser("Alice")
+	sr.users["Hello"] = "Mayank"
+	sr.users["Hi"] = "Bob"
+	sr.Start()
+	go func() { // The following routing is just made to quit the loop for now.
+		time.Sleep(2 * time.Second)
+		//sr.quitch <- struct{}{} // does the same thing as close
+		close(sr.quitch)
+	}()
+	//ch2 := make(chan string, 2)
 
-// 	for i := 0; i > len(sr.users); i++ {
-// 		fmt.Println(sr.users)
-// 	}
-// }
+	fmt.Printf("Printing channel content of new server \n")
+free:
+	for {
+		select {
+		case user, ok := <-sr.userch:
+			if !ok {
+				break // Exit the loop if the channel is closed
+			}
+			fmt.Println(user)
+		default:
+			break free // Exit the loop when the channel is empty
+		}
+	}
+
+	fmt.Println("Printing users map of new server")
+	for users := range sr.users {
+		fmt.Println(users + " " + sr.users[users])
+	}
+
+	// This blocks forever until at least one of the select cases(channels) becomes valid.
+	// A default case can be used to make it non-blocking
+	select {
+	default:
+		fmt.Println("Executing default case to make the select statement non-blocking")
+		break
+	}
+}
 
 func sendMessage(msgCh chan<- string) {
 	msgCh <- " Mayank"
@@ -94,5 +123,5 @@ func sendMessage(msgCh chan<- string) {
 func receiveMessage(msgCh <-chan string) {
 	msg := <-msgCh
 	msg += <-msgCh
-	fmt.Println(msg)
+	fmt.Println("Printing received message " + msg)
 }
